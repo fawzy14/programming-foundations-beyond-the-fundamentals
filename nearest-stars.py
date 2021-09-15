17643340212826f615183f425529e690de8de676
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-15 03:23:33 +0200

    list

---
+++ nearest-stars.py
@@ -0,0 +1,14 @@
+# Nearest stars to Earth 
+star1 = 'Sol'
+star2 = 'Alpha Centauri'
+star3 = 'Barnard'
+star4 = 'Wolf 359'
+
+# fourth nearest star 
+stars = [
+    'Sol',
+    'Alpha Centaurt',
+    'Barnard',
+    'Wolf 395',
+]
+print(stars[3])
