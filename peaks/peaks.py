31de45f29395a89101a3efd77ef866b67a8e3ed0
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-15 04:20:45 +0200

    dictionary

---
+++ peaks/peaks.py
@@ -0,0 +1,20 @@
+# Highest peak on each tectonic plate
+African = 'Kilimanjaro'
+Antarctic = 'Vinson'
+Australian = 'Puncak Jaya'
+Eurasian = 'Everest'
+North_American = 'Denali'
+Pacific = 'Mauna Kea'
+South_American = 'Aconcagua'
+
+# The highest peak on pacific plate 
+peaks = {
+    'African': 'Kilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American':'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua',
+}
+print(peaks['Pacific'])
