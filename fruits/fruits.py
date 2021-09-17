24a904228fcbe35bbfe4fd7af43adbca0b84f72e
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-17 19:45:32 +0200

    for loop

---
+++ fruits/fruits.py
@@ -0,0 +1,11 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+] 
+print('Our fruits selection:')
+for fruit in fruits:
+    print(fruit)
