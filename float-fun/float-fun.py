bbf47b31945dbaefc923b910cb967d3977e9f169
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-19 06:40:50 +0200

    float function

---
+++ float-fun/float-fun.py
@@ -0,0 +1,6 @@
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+print('That value in kilometers is:')
+miles_value = float(miles)
+kilometers_value = miles_value * 1.609344
+print(kilometers_value ,'kilometers')
